b1c72f123ce9f55ee87ea7f5c7ba9fd4c6dd34fd
whitegl0w <Khvoynovsky.V@gmail.com>
2021-07-04 23:53:50 +0300

    added main functionality

--- parse_winget_cli.py
+++ parse_winget_cli.py
@@ -10,14 +10,15 @@ class WinGet(QtCore.QObject):
     data_loaded = QtCore.pyqtSignal(list)
     log_message = QtCore.pyqtSignal(str)
     finished = QtCore.pyqtSignal()
+    load_spinner = QtCore.pyqtSignal(bool)
 
-    def load_updates(self) -> None:
-        self._load_list('upgrade')
-
-    def load_package_list(self) -> None:
-        self._load_list('list')
-
-    def _load_list(self, cmd: str) -> None:
+    @QtCore.pyqtSlot(str)
+    def load_list(self, cmd: str) -> None:
+        self.load_spinner.emit(True)
+        ignore_mode = False
+        if cmd == 'ignore_list':
+            cmd = 'upgrade'
+            ignore_mode = True
         result = str(sbp.run(f'winget {cmd}', stdout=sbp.PIPE, shell=True)
                      .stdout, encoding='utf-8').split('\r\n')
         header = result[0]
@@ -41,8 +42,13 @@ class WinGet(QtCore.QObject):
             app = dict()
             for col in columns:
                 app[col[0]] = re.sub(r'\s{2,}', '', line[col[1]:col[2]])
-            if app['Id'] not in ignore_list:
-                apps.append(app)
+            if ignore_mode:
+                if app['Id'] in ignore_list:
+                    apps.append(app)
+            else:
+                if app['Id'] not in ignore_list:
+                    apps.append(app)
+        self.load_spinner.emit(False)
         self.data_loaded.emit(apps)
 
     @QtCore.pyqtSlot(list)
@@ -52,17 +58,26 @@ class WinGet(QtCore.QObject):
             sbp.run(f'winget upgrade {app}', shell=True)
             self.log_message.emit(f"Upgraded {app}\n")
         self.log_message.emit("\nAll Upgrades Done!!!")
-        self._load_list('upgrade')
+        self.load_list('upgrade')
 
-    @QtCore.pyqtSlot(list)
-    def add_to_ignore(self, apps: list[str]):
+    @QtCore.pyqtSlot(list, str)
+    def change_ignore(self, apps: list[str], mode: str) -> None:
         with open('ignore_list.json', 'r', encoding='UTF-8') as f:
             ignore_list = json.load(f)
-        ignore_list.extend(apps)
+        if mode == 'add':
+            ignore_list.extend(apps)
+        else:
+            for app in apps:
+                ignore_list.remove(app)
         with open('ignore_list.json', 'w', encoding='UTF-8') as f:
             json.dump(ignore_list, f)
-        self.log_message.emit(f'Added to ignore list: {", ".join(apps)}')
-        self._load_list('upgrade')
+
+        if mode == 'add':
+            self.log_message.emit(f'Added to ignore list: {", ".join(apps)}')
+            self.load_list('upgrade')
+        else:
+            self.log_message.emit(f'Deleted from ignore list: {", ".join(apps)}')
+            self.load_list('ignore_list')
 
     @QtCore.pyqtSlot(list)
     def uninstall(self, apps: list[str]) -> None:
@@ -71,4 +86,4 @@ class WinGet(QtCore.QObject):
             sbp.run(f'winget uninstall {app}', shell=True)
             self.log_message.emit(f"Uninstalled {app}\n")
         self.log_message.emit("\nAll Removes Done!!!")
-        self._load_list('upgrade')
+        self.load_list('upgrade')
--- winget_gui.py
+++ winget_gui.py
@@ -1,6 +1,8 @@
 import sys
 
 from PyQt5.QtCore import Qt
+from waitingspinnerwidget import QtWaitingSpinner
+
 from parse_winget_cli import WinGet
 from PyQt5 import QtWidgets, QtCore
 from main_form import Ui_WinGetGui
@@ -9,15 +11,23 @@ from main_form import Ui_WinGetGui
 class MyWindow(QtWidgets.QMainWindow, QtCore.QObject):
     upgrade_apps = QtCore.pyqtSignal(list)
     remove_apps = QtCore.pyqtSignal(list)
-    add_to_ignore = QtCore.pyqtSignal(list)
+    change_ignore = QtCore.pyqtSignal(list, str)
+    load_app_list = QtCore.pyqtSignal(str)
 
     def __init__(self):
         super(MyWindow, self).__init__()
         self.ui = Ui_WinGetGui()
         self.ui.setupUi(self)
-        self.show()
+        # загрузка таблицы стилей
+        with open('Ubuntu.qss', 'r') as f:
+            self.setStyleSheet(f.read())
         # настройка интерфейса
-
+        self.spinner = QtWaitingSpinner(self)
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.spinner)
+        self.ui.tableWidget.setLayout(layout)
+        self.mode = 'upgrade'
+        self.ui.rb_updates.setChecked(True)
         # запуск загрузки в новом потоке
         self.thread = QtCore.QThread()
         self.winget_cmd = WinGet()
@@ -25,24 +35,47 @@ class MyWindow(QtWidgets.QMainWindow, QtCore.QObject):
         self.winget_cmd.data_loaded.connect(self.refresh_list)
         self.winget_cmd.finished.connect(self.quit_thread)
         self.winget_cmd.log_message.connect(self.print_log)
-        self.thread.started.connect(self.winget_cmd.load_updates)
+        self.winget_cmd.load_spinner.connect(self.load_spinner)
+        self.thread.started.connect(lambda: self.load_app_list.emit(self.mode))
         self.thread.start()
         # привязка слотов
         self.upgrade_apps.connect(self.winget_cmd.upgrade)
         self.remove_apps.connect(self.winget_cmd.uninstall)
-        self.add_to_ignore.connect(self.winget_cmd.add_to_ignore)
+        self.change_ignore.connect(self.winget_cmd.change_ignore)
+        self.load_app_list.connect(self.winget_cmd.load_list)
         # настройка кнопок
-        self.ui.pb_refresh.clicked.connect(self.winget_cmd.load_updates)
+        self.ui.pb_refresh.clicked.connect(lambda: self.load_app_list.emit(self.mode))
         self.ui.pb_update.clicked.connect(self.upgrade_apps_fn)
         self.ui.pb_remove.clicked.connect(self.remove_apps_fn)
         self.ui.pb_add_to_ignore.clicked.connect(self.add_to_ignore_fn)
+        self.ui.rb_ignore_list.clicked.connect(self.change_mode)
+        self.ui.rb_installed.clicked.connect(self.change_mode)
+        self.ui.rb_updates.clicked.connect(self.change_mode)
+        # отображение окна
+        self.show()
+
+    def change_mode(self):
+        self.ui.pb_add_to_ignore.setText('Add to ignore list')
+        self.ui.pb_add_to_ignore.setEnabled(True)
+        if self.ui.rb_updates.isChecked():
+            self.mode = 'upgrade'
+        elif self.ui.rb_installed.isChecked():
+            self.mode = 'list'
+            self.ui.pb_add_to_ignore.setEnabled(False)
+        elif self.ui.rb_ignore_list.isChecked():
+            self.mode = 'ignore_list'
+            self.ui.pb_add_to_ignore.setText('Delete from ignore list')
+        self.load_app_list.emit(self.mode)
 
     def add_to_ignore_fn(self):
         apps = []
         for i in range(self.ui.tableWidget.rowCount()):
             if self.ui.tableWidget.item(i, 0).checkState():
                 apps.append(self.ui.tableWidget.item(i, 0).text())
-        self.add_to_ignore.emit(apps)
+        if self.mode == 'ignore_list':
+            self.change_ignore.emit(apps, 'delete')
+        else:
+            self.change_ignore.emit(apps, 'add')
 
     def upgrade_apps_fn(self):
         apps = []
@@ -57,6 +90,15 @@ class MyWindow(QtWidgets.QMainWindow, QtCore.QObject):
             if self.ui.tableWidget.item(i, 0).checkState():
                 apps.append(self.ui.tableWidget.item(i, 0).text())
         self.remove_apps.emit(apps)
+
+    @QtCore.pyqtSlot(bool)
+    def load_spinner(self, status: bool):
+        if status:
+            self.ui.tableWidget.setRowCount(0)
+            self.spinner.start()
+        else:
+            self.ui.horizontalLayout.setEnabled(True)
+            self.spinner.stop()
 
     @QtCore.pyqtSlot()
     def quit_thread(self):
